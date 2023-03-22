dd78f3dfac0b344d0477eb895973adbacbd21faf
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2023-03-22 23:23:52 +0800

    Update prepare-data.sh

--- examples/med_diag/scripts/prepare-data.sh
+++ examples/med_diag/scripts/prepare-data.sh
@@ -19,14 +19,24 @@ cut -f2 -d"," $infolder/preliminary_a_test.csv > $outfolder/test.src
 
 echo "train dev split..."
 python nmt_data_tools/my_tools/train_dev_split.py src tgt train $outfolder  500
-mv $outfolder/dev.src  $outfolder/valid.src
-mv $outfolder/dev.tgt  $outfolder/valid.tgt
+mv $outfolder/dev.src  $outfolder/valid.$src
+mv $outfolder/dev.tgt  $outfolder/valid.$tgt
 
 
 echo "build vocab..."
 # -e开启转义
-echo -e "<s>\n<pad>\n</s>\n<unk>" > $outfolder/vocab.src
-for i in {9..1299}; do echo $i >> $outfolder/vocab.src; done
+#echo -e "<s>\n<pad>\n</s>\n<unk>" > $outfolder/vocab.src
+#for i in {9..1299}; do echo $i >> $outfolder/vocab.src; done
+
+echo "<s>" > $outfolder/vocab.src
+echo "<pad>" >> $outfolder/vocab.src
+echo "</s>" >> $outfolder/vocab.src
+echo "<unk>" >> $outfolder/vocab.src
+for i in $(seq 9 1299)
+do
+  echo $i >> $outfolder/vocab.src
+done
+
 cp $outfolder/vocab.src $outfolder/vocab.tgt
 
 ls $outfolder
